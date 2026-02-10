12a9efd5d2882f7888bb8bf85ad73361d20759e3
Chirag Jain <cjain9975@gmail.com>
2026-02-10 09:24:15 +0530

    Python CI/CD Pipeline getting started

---
+++ src/main.py
@@ -0,0 +1,9 @@
+
+def addTwoNumbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 20
+    result = addTwoNumbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+from src.main import addTwoNumbers
+
+def test_addTwoNumbers():
+    assert addTwoNumbers(2, 3) == 5
+    assert addTwoNumbers(-1, 1) == 0
+    assert addTwoNumbers(0, 0) == 0
+    assert addTwoNumbers(1.5, 2.5) == 4.0
