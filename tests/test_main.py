ecc8f389cf07348789c399b6a847cf32a1ade23a
Chirag Jain <cjain9975@gmail.com>
2026-02-10 09:46:34 +0530

    Python CI/CD Pipeline getting started

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,4 @@ def test_addTwoNumbers():
     assert addTwoNumbers(2, 3) == 5
     assert addTwoNumbers(-1, 1) == 0
     assert addTwoNumbers(0, 0) == 0
-    assert addTwoNumbers(1.5, 2.5) == 4.1
+    assert addTwoNumbers(1.5, 2.5) == 4.0
